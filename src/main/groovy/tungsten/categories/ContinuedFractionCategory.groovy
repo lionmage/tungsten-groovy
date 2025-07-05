1b393ab9cff82e7b8e4adcd82877f5947bb95f84
Robert Poole <Tarquin.AZ@gmail.com>
2025-07-04 23:16:12 -0700

    Enhancements to Number

--- src/main/groovy/tungsten/categories/ContinuedFractionCategory.groovy
+++ src/main/groovy/tungsten/categories/ContinuedFractionCategory.groovy
@@ -2,6 +2,7 @@ package tungsten.categories
 
 import org.codehaus.groovy.runtime.StringGroovyMethods
 import tungsten.types.numerics.impl.ContinuedFraction
+import tungsten.types.numerics.impl.RealImpl
 
 class ContinuedFractionCategory {
     static def asType(List self, Class clazz) {
@@ -22,5 +23,49 @@ class ContinuedFractionCategory {
             return new ContinuedFraction(self)
         }
         return conversion(self, clazz)
+    }
+
+    /*
+     Enhancements for Number
+     */
+
+    static def plus(Number self, ContinuedFraction operand) {
+        if (self instanceof Integer || self instanceof Long || self instanceof BigInteger) {
+            ContinuedFraction converted = new ContinuedFraction(List.of(self.toLong()))
+            return converted.add(operand)
+        }
+        def decConverted = self.toBigDecimal()
+        ContinuedFraction cf = new ContinuedFraction(new RealImpl(decConverted, operand.getMathContext(), false))
+        return cf.add(operand)
+    }
+
+    static def minus(Number self, ContinuedFraction operand) {
+        if (self instanceof Integer || self instanceof Long || self instanceof BigInteger) {
+            ContinuedFraction converted = new ContinuedFraction(List.of(self.toLong()))
+            return converted.subtract(operand)
+        }
+        def decConverted = self.toBigDecimal()
+        ContinuedFraction cf = new ContinuedFraction(new RealImpl(decConverted, operand.getMathContext(), false))
+        return cf.subtract(operand)
+    }
+
+    static def multiply(Number self, ContinuedFraction operand) {
+        if (self instanceof Integer || self instanceof Long || self instanceof BigInteger) {
+            ContinuedFraction converted = new ContinuedFraction(List.of(self.toLong()))
+            return converted.multiply(operand)
+        }
+        def decConverted = self.toBigDecimal()
+        ContinuedFraction cf = new ContinuedFraction(new RealImpl(decConverted, operand.getMathContext(), false))
+        return cf.multiply(operand)
+    }
+
+    static def div(Number self, ContinuedFraction operand) {
+        if (self instanceof Integer || self instanceof Long || self instanceof BigInteger) {
+            ContinuedFraction converted = new ContinuedFraction(List.of(self.toLong()))
+            return converted.divide(operand)
+        }
+        def decConverted = self.toBigDecimal()
+        ContinuedFraction cf = new ContinuedFraction(new RealImpl(decConverted, operand.getMathContext(), false))
+        return cf.divide(operand)
     }
 }
